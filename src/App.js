db60e47dde244d77c7dffd82d2b8e3002f69ac07
kushaal-rana <kurana@cs.stonybrook.edu>
2024-09-01 14:19:11 -0400

    Add About Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <SocialLinks />
+      <About/>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full md:h-[110%] ">
+        <div className="">
+          <p className="text-4xl font-bold inline border b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          repellendus mollitia quasi, adipisci maxime enim possimus totam eius
+          et quas quis corporis exercitationem repellat nemo in sint sequi
+          saepe. Nam? Voluptatibus officiis illo modi nostrum dolor assumenda,
+          sequi in fugit accusamus? Obcaecati aperiam dicta blanditiis, eligendi
+          mollitia molestias repellat aliquam similique doloremque voluptatibus
+          qui repudiandae. Est aperiam aliquid eius expedita. Tempora aperiam
+          perspiciatis laudantium aut, officiis libero eaque cum deserunt ipsam
+          ex! Nulla totam corrupti facere, maiores fugiat itaque repudiandae
+          reiciendis possimus facilis ab libero id excepturi consequuntur
+          reprehenderit ex.
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est rerum
+          repudiandae tempora quis omnis! Asperiores eveniet dolorem sunt, ut
+          dolor incidunt necessitatibus placeat in fuga culpa earum ea odit
+          tempore consequuntur cupiditate sint, nobis ab! Nesciunt, sunt. Nobis
+          eum nemo obcaecati, perspiciatis, voluptatem vitae fugiat ipsum iusto
+          neque modi saepe!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
