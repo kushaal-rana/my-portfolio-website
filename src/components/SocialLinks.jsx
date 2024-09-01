798d55a1fd9b20dd50adb1921980ec60e9672be7
kushaal-rana <kurana@cs.stonybrook.edu>
2024-09-01 12:44:18 -0400

    Added SocialLinks component

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {FaGithub, FaLinkedin} from "react-icons/fa";
 import { HiOutlineMail} from "react-icons/hi";
-import { BsFillPersonalLinesFill } from "react-icons/bs";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
   const links = [
@@ -37,26 +37,35 @@ const SocialLinks = () => {
       id:4,
       child: (
             <>
-            Resume <BsFillPersonalLinesFill size={30}/>
+            Resume <BsFillPersonLinesFill size={30}/>
             </>
       ),
       href: 'https://drive.google.com/file/d/16ZP5CcgCQ4gkRm21cv46p-SnSmhRbTXG/view?usp=sharing',
-      style: 'rounded-bl-md',
+      style: 'rounded-br-md',
       download:true,
     }
-  ]
+  ];
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'>
-      
-          <a href="" className="flex justify-between items-center w-full text-white">
-           
-          </a>
-        </li>
+        {links.map(({id,child, href, style, download}) => (
+          <li 
+          key={id} 
+          className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style}`}>
+           <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default SocialLinks
