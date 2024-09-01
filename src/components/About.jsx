3fcb6ba73103e314a6729dcc48887c8098a11149
kushaal-rana <kurana@cs.stonybrook.edu>
2024-09-01 15:33:35 -0400

    css change in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full md:h-[110%] ">
         <div className="">
-          <p className="text-4xl font-bold inline border b-4 border-gray-500">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
