cc22751dc6bb03e8ce82243f28819d76b9600aeb
kushaal-rana <kurana@cs.stonybrook.edu>
2024-09-07 00:01:27 -0400

    Added Animation to text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,28 +2,57 @@ import React from "react";
 import HeroImage from "../assets/heroImage.jpeg";
 import { RiArrowRightSFill } from "react-icons/ri";
 import { Link } from "react-scroll";
+import { TypeAnimation } from "react-type-animation";
+
+const Details = {
+  name: "Kushaal Rana",
+  roles: [
+    "Full Stack Developer",
+    "Backend Developer",
+    "Frontend Designer",
+    "Software Engineer",
+    "Software Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/kushaal-rana",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/kushaal-rana/",
+  insta: "https://www.instagram.com/kushaalrana/",
+};
 
 const Home = () => {
   return (
     <div
       name="home"
-      className="h-fit md:h-screen w-full bg-gradient-to-b from-black to-gray-800 pt-20 md:pt-2"
+      className="h-fit md:h-screen w-full bg-gradient-to-b from-black to-gray-800 pt-20 md:pt-2 relative overflow-hidden"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-5 md:flex-row">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-5 md:flex-row relative z-10">
         {/* Image - now first on mobile */}
-        <div className="order-1 md:order-2 mb-8 md:mb-0 ">
+        <div className="order-1 md:order-2 mb-8 md:mb-0 mt-12 md:mt-0 ">
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl w-[300px] h-auto md:w-[500px] mx-auto"
+            className="rounded-2xl w-[300px] h-auto md:w-[410px] mx-auto object-cover border-4 border-violet-800 shadow-lg shadow-indigo-500/50 transition-all duration-500 hover:scale-105 hover:shadow-blue-500"
           />
         </div>
 
         {/* Text content - now second on mobile */}
         <div className="flex flex-col justify-center h-full order-2 md:order-1 mt-8 md:mt-0">
           <h2 className="text-4xl sm:text-6xl font-bold text-white text-center md:text-left">
-            I'm a Full Stack Developer
+            Hi, I'm {Details.name}
           </h2>
+          <div className="text-2xl sm:text-3xl font-semibold text-purple-600 text-center md:text-left mb-4">
+            A{" "}
+            <TypeAnimation
+              sequence={[...Details.roles.flatMap((role) => [role, 1000])]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </div>
           <p className="text-slate-500 font-normal text-lg py-4 max-w-md text-center md:text-left">
             I turn ideas into seamless digital experiences with clean, efficient
             code. Passionate about building innovative solutions that solve
